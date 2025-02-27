fc8f1a9bb4e6ebd322117efa393fa079183bf2d9
Sylvain-coder <007sylvain@gmail.com>
2025-02-27 14:56:53 +0100

    Initial commit

---
+++ src/test/java/la/sy/contacts/ContactsApplicationTests.java
@@ -0,0 +1,13 @@
+package la.sy.contacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
